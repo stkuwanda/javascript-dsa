d9b64b093897993d4bfafec170f18486ff903383
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-27 13:04:16 +0200

    the idea of good code

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('run...');
+// Good code should be 1. readable 2. scalable
+
+//#1 -- For loop in Javascript.
+const nemo = ['nemo'];
+
+function findNemo1(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+}
+
+findNemo1(nemo);
