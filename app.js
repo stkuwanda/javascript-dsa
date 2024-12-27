5a86b8bccb4f64b787ada49dc537a2ee32a57ce3
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-27 19:25:49 +0200

    linear time O(n)

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ const nemo = ['nemo'];
 const large = new Array(200).fill('nemo');
 
 function findNemo1(array) {
-  let t0 = performance.now(), t1;
+  // let t0 = performance.now(), t1;
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
 
-  t1 = performance.now();
-  console.log(`Call to find Nemo took ${t1 - t0} ms.`);
+  // t1 = performance.now();
+  // console.log(`Call to find Nemo took ${t1 - t0} ms.`);
 }
 
 findNemo1(nemo);
-findNemo1(large);
+findNemo1(large); // O(n) ---> Linear Time
+
