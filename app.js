dd63c3e2e21447a2629807036d32651e8810594a
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-27 13:48:44 +0200

    intro to Big O analysis

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 console.log('run...');
 // Good code should be 1. readable 2. scalable
+// Big O is a way to measure the time complexity and/or the number of operations and space complexity of a program, inorder to assess
+// code scalability and algorithmic efficiency
+// 
 
 //#1 -- For loop in Javascript.
 const nemo = ['nemo'];
+const large = new Array(200).fill('nemo');
 
 function findNemo1(array) {
+  let t0 = performance.now(), t1;
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
+
+  t1 = performance.now();
+  console.log(`Call to find Nemo took ${t1 - t0} ms.`);
 }
 
 findNemo1(nemo);
+findNemo1(large);
