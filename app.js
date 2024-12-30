8a6e093571628b37d37c1a2cbd79b6c53e500432
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-30 06:57:50 +0200

    Big O Shorthands extra notes

--- app.js
+++ app.js
@@ -105,3 +105,9 @@ function arrayOfHiNTimes(n) {
 
 arrayOfHiNTimes(6);
 
+// Big O Shorthands
+// 1. Arithmetic operations are constant
+// 2. Variable assignment is constant
+// 3. Accessing elements in an array (by index) or object (by key) is constant.
+// 4. In a loop, the complexity is the length of the loop times the complexity
+// of whatever happens inside the loop
